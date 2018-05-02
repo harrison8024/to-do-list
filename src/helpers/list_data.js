e7de1822f0f687f67090f1fed5072f8823397625
harrison8024 <harrison8024@gmail.com>
2018-05-02 16:23:48 -0700

    Completed display dummy data

---
+++ src/helpers/list_data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Learn React',
+        details: 'Learn reactJS'
+    },
+    {
+        title: 'Learn BackEnd',
+        details: 'Learn PhP'
+    },
+    {
+        title: 'Do prototype',
+        details: 'Mysqli function'
+    },
+    {
+        title: 'Eat Dinner',
+        details: 'Left overs'
+    }
+];
