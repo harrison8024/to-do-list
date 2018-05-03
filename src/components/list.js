4b67d71425471cf8d2fcd298b3b0451d8faa7caf
harrison8024 <harrison8024@gmail.com>
2018-05-02 18:47:30 -0700

    finish list data and add items

--- src/components/list.js
+++ src/components/list.js
@@ -2,17 +2,16 @@ import React from 'react';
 import listData from '../helpers/list_data';
 
 export default props => {
-    console.log("List Data",listData);
+    if(!props.data.length){
+        return <h1 className="center grey-text text-lighten-2">No To Do Items</h1>
+    }
 
-    const listElements = listData.map((item,index)=>{
+    const listElements = props.data.map((item,index)=>{
         return <li key={index} className="collection-item">{item.title}</li>
     });
     return (
-        <div>
-            <h3>List will be here</h3>
-            <ul className="collection">
-                {listElements}
-            </ul>
-        </div>
+        <ul className="collection">
+            {listElements}
+        </ul>
     )
 }
